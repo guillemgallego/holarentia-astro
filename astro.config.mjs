8bbeeb3e3a26dbf8552fac64a1da27561fec39df
guillemgallego <guillemgallego@users.noreply.github.com>
2026-03-11 20:29:04 +0100

    feat: Add Analytics SEO Improvements and Layout Refactoring

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://holarentia.com',
   trailingSlash: 'ignore',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
