3102e9a344a2ba17383dd61427f12ca463f982ae
guillemgallego <guillemgallego@users.noreply.github.com>
2026-04-13 22:41:09 +0200

    Fix: Configurado el adaptador de Cloudflare para el Chatbot IA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
   integrations: [tailwind(), sitemap()],
   site: 'https://holarentia.com',
 });
